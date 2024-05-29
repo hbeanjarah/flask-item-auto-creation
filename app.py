7e96e6856a58b43206e29d02b67619c5b362a39d
Be Anjara Houssen <55980797+hbeanjarah@users.noreply.github.com>
2024-05-29 05:42:46 +0300

    :sparkles: get and post integration (#1)

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+core_items = [
+    {"id": 1, "name": "Marketing Professionals"},
+    {"id": 2, "name": "Software Engineers"},
+    {"id": 3, "name": "Human Resources Managers"},
+    {"id": 4, "name": "Sales Executives"},
+    {"id": 5, "name": "Financial Analysts"},
+    {"id": 6, "name": "Project Managers"},
+    {"id": 7, "name": "Data Scientists"},
+    {"id": 8, "name": "Healthcare Administrators"},
+    {"id": 9, "name": "IT Support Specialists"},
+    {"id": 10, "name": "Product Managers"},
+]
 
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"id": 1})
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+@app.route("/core_items", methods=["GET", "POST"])
+def get_core_itmes():
+    if request.method == "GET":
+        if len(core_items) > 0:
+            return jsonify(core_items)
+        else:
+            return "Not Found", 400
+    if request.method == "POST":
+        app.logger.info(request.form["name"])
+        new_item = request.form["name"]
+        last_id = core_items[-1]["id"]
+        core_items.append({"id": last_id + 1, "name": new_item})
+        return jsonify(core_items), 201
